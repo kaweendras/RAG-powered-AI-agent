5820f0ac43825c5e6769331fb58d0354fe9dfd64
kaweendras <salitha.wijerathna@gmail.com>
2025-05-22 12:15:23 +0530

    CHG | add Google AI model configuration to the config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -9,4 +9,9 @@ export const config = {
   EMBEDDINGS_MODEL: process.env.EMBEDDINGS_MODEL || "nomic-embed-text:v1.5",
   OLLAMA_API_URL: process.env.OLLAMA_API_URL || "http://localhost:11434/api",
   OLLAMA_MODEL: process.env.OLLAMA_MODEL || "llama3.2:latest",
+  GOOGLEAI_MODEL_NAME: process.env.GOOGLEAI_MODEL_NAME || "gemini-2.0-flash",
+  GOOGLEAI_API_KEY: process.env.GOOGLEAI_API_KEY || "YOUR_GOOGLEAI_API_KEY",
+  GOOGLEAI_API_URL:
+    process.env.GOOGLEAI_API_URL ||
+    "https://generativelanguage.googleapis.com/v1beta/models/",
 };
