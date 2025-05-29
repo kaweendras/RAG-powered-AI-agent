c1627f53b46926b89ed0673400d31ce91079a0ae
kaweendras <salitha.wijerathna@gmail.com>
2025-05-29 13:49:15 +0530

    CHG | Refactor aiQueryController to support dynamic model selection based on query mode

--- src/controllers/queryController.ts
+++ src/controllers/queryController.ts
@@ -1,19 +1,24 @@
-// filepath: /home/kaweendra/work/per/RAG-powered-AI-agent/src/controllers/queryController.ts
 import { generateGoogleAIResponse } from "../llmClients/googleClient/queryClientGoogle";
-/**
- * Handle queries to the Google AI model
- * @param req Express request object
- * @param res Express response object
- */
-export const queryGoogleAI = async (req: any, res: any) => {
+import { GenerateOllamaResponse } from "../llmClients/ollamaClient/queryClientOllama";
+
+export const aiQueryController = async (req: any, res: any) => {
   try {
     const { query } = req.body;
+
+    //catch mode parameter from query
+    const mode = req.query.mode || "ollama";
+    console.log("Mode:", mode);
+
+    const MODEL =
+      mode.toLowerCase() === "ollama"
+        ? GenerateOllamaResponse
+        : generateGoogleAIResponse;
 
     if (!query) {
       return res.status(400).json({ error: "Query parameter is required" });
     }
 
-    const response = await generateGoogleAIResponse(query);
+    const response = await MODEL(query);
     res.json({ response });
   } catch (error) {
     console.error("Error processing Google AI query:", error);
